01163ffa4bd8250e48f012e3dfc9a81052a87726
Daniel Sweet <dsweet@zhealthdocumentation.com>
2017-03-28 22:34:54 -0700

    Add custom fields to tutorial post type

--- wp-content/themes/etchasketch/metaboxes/tutorial-meta.php
+++ wp-content/themes/etchasketch/metaboxes/tutorial-meta.php
@@ -3,117 +3,86 @@
 	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 	Cras orci lorem, bibendum in pharetra ac, luctus ut mauris.</p>
 
-	<label>Name</label>
+	<div class="meta-label">
+		<label>Video Link</label>
+	</div><!--.meta-label-->
 
-	<p>
-		<input type="text" name="<?php $metabox->the_name('name'); ?>" value="<?php $metabox->the_value('name'); ?>"/>
-		<span>Enter in a name</span>
-	</p>
+	<div class="meta-input">
+		<input type="text" name="<?php $metabox->the_name('tut-vimeo'); ?>" value="<?php $metabox->the_value('name'); ?>"/>
+		<span>Paste Vimeo URL, example: xxxx</span>
+	</div><!--.meta-label-->
 
-	<label>Description <span>(optional)</span></label>
+	<?php global $wpalchemy_media_access; ?>
 
-	<p>
-		<?php $metabox->the_field('description'); ?>
+	<div class="meta-label">
+		<label>PDF version for download</label>
+	</div><!--.meta-label-->
+
+	<div class="meta-input">
+		<?php $mb->the_field('imgurl'); ?>
+		<?php $wpalchemy_media_access->setGroupName('doc-n'. $mb->get_the_index())->setInsertButtonLabel('Insert'); ?>
+		<?php echo $wpalchemy_media_access->getField(array('name' => $mb->get_the_name(), 'value' => $mb->get_the_value())); ?>
+		<?php echo $wpalchemy_media_access->getButton( array( 'label' => 'Select File' ) ); ?>
+	</div><!--.meta-label-->
+
+	<div class="meta-label">
+		<label>Walkthrough Description</label>
+	</div><!--.meta-label-->
+
+	<div class="meta-input">
+		<?php $metabox->the_field('wt-description'); ?>
 		<textarea name="<?php $metabox->the_name(); ?>" rows="3"><?php $metabox->the_value(); ?></textarea>
 		<span>Enter in a description</span>
-	</p>
-
-	<h4>Documents</h4>
-
-	<a style="float:right; margin:0 10px;" href="#" class="dodelete-docs button">Remove All</a>
-
-	<p>Add documents to the library by entering in a title,
-	URL and selecting a level of access. Upload new documents
-	using the "Add Media" box.</p>
-
-	<?php while($mb->have_fields_and_multi('docs')): ?>
-	<?php $mb->the_group_open(); ?>
-
-		<?php $mb->the_field('title'); ?>
-		<label>Title and URL</label>
-		<p><input type="text" name="<?php $mb->the_name(); ?>" value="<?php $mb->the_value(); ?>"/></p>
-
-		<?php $mb->the_field('link'); ?>
-		<p><input type="text" name="<?php $mb->the_name(); ?>" value="<?php $mb->the_value(); ?>"/></p>
-
-		<?php $mb->the_field('access'); ?>
-		<p><strong>Access:</strong>
-			<input type="radio" name="<?php $mb->the_name(); ?>" value="admin"<?php echo $mb->is_value('admin')?' checked="checked"':''; ?>/> Admin
-			<input type="radio" name="<?php $mb->the_name(); ?>" value="editor"<?php echo $mb->is_value('editor')?' checked="checked"':''; ?>/> Editor
-			<input type="radio" name="<?php $mb->the_name(); ?>" value="subscriber"<?php echo $mb->is_value('subscriber')?' checked="checked"':''; ?>/> Subscriber
-
-			<a href="#" class="button" style="margin-left:10px;" onclick="jQuery(this).siblings().removeAttr('checked'); return false;">Remove Access</a>
-			<a href="#" class="dodelete button">Remove Document</a>
-		</p>
-
-	<?php $mb->the_group_close(); ?>
-	<?php endwhile; ?>
-
-	<p style="margin-bottom:15px; padding-top:5px;"><a href="#" class="docopy-docs button">Add Document</a></p>
+	</div><!--.meta-field-->
 
 
 	<section>
-			<h2><i class="fa fa-list-ol"></i> &nbsp;Fast Facts</h2>
-			<p>Multiple sets can be added if there are more than one program within this department.</p>
 
-		<!--	<?php //$mb->the_field('ff-slider-check'); ?>
-			<p><input type="checkbox" name="<?php $mb->the_name(); ?>" value="slider"<?php $mb->the_checkbox_state('slider'); ?>/> <label>Use slider</label></p>
-	-->
-
-			<p class="sort-warning"><i>Remember to save after ordering items!</i></p>
-
-			<?php while($mb->have_fields_and_multi('fast-facts')): ?>
+			<?php while($mb->have_fields_and_multi('wt-steps')): ?>
 			<?php $mb->the_group_open(); ?>
 		 		<div class="content">
 
 			 		<div class="repeating-heading">
-						<h4>Fast Fact Group&nbsp;&nbsp;<i class="fa fa-arrows"></i></h4>
+						<h4>Walkthrough Steps&nbsp;&nbsp;<i class="fa fa-arrows"></i></h4>
 						<a href="#" class="dodelete button">Remove</a>
 					</div><!--.repeating-heading-->
 
 					<div class="meta-label">
-						<label>Group Heading</label>
+						<label>Step Title</label>
 					</div><!--.meta-label-->
 
 					<div class="meta-input">
-						<?php $mb->the_field('lp-ff-heading'); ?>
-						<input type="text" name="<?php $mb->the_name(); ?>" value="<?php $mb->the_value(); ?>"/>
+						<input type="text" name="<?php $metabox->the_name('tut-vimeo'); ?>" value="<?php $metabox->the_value('wt-step-title'); ?>"/>
 						<span></span>
-					</div><!--.meta-input-->
-
-					<div class="meta-label">
-						<label>Program</label>
 					</div><!--.meta-label-->
 
-				 	<div class="meta-input">
-						<p>
-							<?php $mb->the_field('lp-ff-program'); ?>
-							<input type="text" name="<?php $mb->the_name(); ?>" value="<?php $mb->the_value(); ?>"/>
-							<span></span>
-						</p>
+					<div class="meta-label">
+						<label>Step Description</label>
+					</div><!--.meta-label-->
+
+					<div class="meta-input">
+						<?php $metabox->the_field('wt-step-description'); ?>
+						<textarea name="<?php $metabox->the_name(); ?>" rows="3"><?php $metabox->the_value(); ?></textarea>
+						<span></span>
 					</div><!--.meta-field-->
 
 					<div class="meta-label">
-						<label>Format</label>
+						<label>Screenshot</label>
 					</div><!--.meta-label-->
 
-				 	<div class="meta-input">
-						<p>
-							<?php $mb->the_field('lp-ff-format'); ?>
-							<input type="text" name="<?php $mb->the_name(); ?>" value="<?php $mb->the_value(); ?>"/>
-							<span></span>
-						</p>
-					</div><!--.meta-field-->
+					<div class="meta-input">
+						<?php $mb->the_field('wt-screenshot'); ?>
+						<?php $wpalchemy_media_access->setGroupName('img-n'. $mb->get_the_index())->setInsertButtonLabel('Insert'); ?>
+						<?php echo $wpalchemy_media_access->getField(array('name' => $mb->get_the_name(), 'value' => $mb->get_the_value())); ?>
+						<?php echo $wpalchemy_media_access->getButton( array( 'label' => 'Select File' ) ); ?>
+					</div><!--.meta-label-->
 
-					
 				</div><!--.content-->
 
 			<?php $mb->the_group_close(); ?>
 			<?php endwhile; ?>
 
 			<p style="margin-bottom:15px; padding-top:5px;"><a href="#" class="docopy-fast-facts add button"><i class="fa fa-plus-circle"></i> Add Another</a></p>
-
-
 
 		</section>
 </div>
--- wp-content/wpalchemy/MediaAccess.php
+++ wp-content/wpalchemy/MediaAccess.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @author   	Dimas Begunoff
  * @copyright	Copyright (c) 2011, Dimas Begunoff, http://farinspace.com/
@@ -9,7 +8,6 @@
  * @link     	http://github.com/farinspace/wpalchemy/
  * @link     	http://farinspace.com/
  */
-
  class WPAlchemy_MediaAccess
 {
 	/**
@@ -21,7 +19,6 @@
 	 * @var		string required
 	 */
 	public $button_class_name = 'mediabutton';
-
 	/**
 	 * User defined identifier for the css class name of the HTML field element,
 	 * used when pairing the field and button elements
@@ -31,7 +28,6 @@
 	 * @var		string required
 	 */
 	public $field_class_name = 'mediafield';
-
 	/**
 	 * User defined label for the insert button in the media upload box, this
 	 * can be set once or per field and button pair.
@@ -42,7 +38,16 @@
 	 * @see		setInsertButtonLabel()
 	 */
 	public $insert_button_label = null;
-
+	/**
+	 * User defined label for the "Add Media" button, this
+	 * can be set once or per field and button pair.
+	 *
+	 * @since	0.1
+	 * @access	public
+	 * @var		string optional
+	 * @see		setInsertButtonLabel()
+	 */
+	public $button_label = null;
 	/**
 	 * Used to track the current groupname for pairing a field and button.
 	 *
@@ -52,7 +57,6 @@
 	 * @see		setGroupName()
 	 */
 	private $groupname = null;
-
 	/**
 	 * Used to track the current tab for the media upload box.
 	 *
@@ -62,7 +66,6 @@
 	 * @see		setTab()
 	 */
 	private $tab = null;
-
 	/**
 	 * MediaAccess class
 	 *
@@ -80,11 +83,10 @@
 		if ( ! defined('WPALCHEMY_SEND_TO_EDITOR_ENABLED'))
 		{
 			add_action('admin_footer', array($this, 'init'));
-
 			define('WPALCHEMY_SEND_TO_EDITOR_ENABLED', true);
 		}
-	}
 
+	}
 	/**
 	 * Used to generate short unique/random names
 	 *
@@ -96,7 +98,6 @@
 	{
 		return substr(md5(microtime() . rand()), rand(0,25), 6);
 	}
-
 	/**
 	 * Used to set the insert button label in the media upload box, this can be
 	 * set once or per field and button pair.
@@ -110,17 +111,19 @@
 	public function setInsertButtonLabel($label = 'Insert')
 	{
 		$this->insert_button_label = $label;
-
 		return $this;
 	}
 
+	public function setButtonLabel($button = 'Add Media')
+	{
+		$this->button_label = $button;
+		return $this;
+	}
 	public function setTab($name)
 	{
 		$this->tab = $name;
-
 		$this;
 	}
-
 	/**
 	 * Used before calls to getField(), getButton() or getButtonClass() to set
 	 * the groupname to pair a field and button element.
@@ -134,10 +137,8 @@
 	public function setGroupName($name)
 	{
 		$this->groupname = $name;
-
 		return $this;
 	}
-
 	/**
 	 * Used to insert a form field of type "text", this should be paired with a
 	 * button element. The name and value attributes are required.
@@ -151,36 +152,27 @@
 	public function getField(array $attr)
 	{
 		$groupname = isset($attr['groupname']) ? $attr['groupname'] : $this->groupname ;
-		
+
 		$attr_default = array
 		(
 			'type' => 'text',
 			'class' => $this->field_class_name . '-' . $groupname,
 		);
-
 		###
-
 		if (isset($attr['class']))
 		{
 			$attr['class'] = $attr_default['class'] . ' ' . trim($attr['class']);
 		}
-
 		$attr = array_merge($attr_default, $attr);
-
 		###
-
 		$elem_attr = array();
-
 		foreach ($attr as $n => $v)
 		{
 			array_push($elem_attr, $n . '="' . $v . '"');
 		}
-
 		###
-
 		return '<input ' . implode(' ', $elem_attr) . '/>';
 	}
-
 	/**
 	 * Used to get the link used for the button element. If creating custom
 	 * buttons, this method should be used to get the link needed for proper
@@ -196,14 +188,18 @@
 	{
 		// this is set even for new posts/pages
 		global $post_ID; //wp
-
 		$tab = ! empty($tab) ? $tab : $this->tab ;
-
 		$tab = ! empty($tab) ? $tab : 'library' ;
-		
-		return 'media-upload.php?post_id=' . $post_ID . '&tab=' . $tab . '&TB_iframe=1';
-	}
 
+		$update_href = esc_url( add_query_arg( array(
+			'post_id' => $post_ID,
+			'tab' => $tab,
+			'TB_iframe' => 1,
+			'_wpnonce' => wp_create_nonce('shiba_gallery_options'),
+			), admin_url('upload.php') ) );
+
+		return $update_href;
+	}
 	/**
 	 * Used to get the CSS class name(s) used for the button element. If
 	 * creating custom buttons, this method should be used to get the css class
@@ -218,10 +214,9 @@
 	public function getButtonClass($groupname = null)
 	{
 		$groupname = isset($groupname) ? $groupname : $this->groupname ;
-		
-		return $this->button_class_name . '-' . $groupname . ' thickbox';
-	}
 
+		return $this->button_class_name . '-' . $groupname . ' ';
+	}
 	/**
 	 * Used to get the CSS class name used for the field element. If
 	 * creating a custom field, this method should be used to get the css class
@@ -236,10 +231,8 @@
 	public function getFieldClass($groupname = null)
 	{
 		$groupname = isset($groupname) ? $groupname : $this->groupname ;
-
 		return $this->field_class_name . '-' . $groupname;
 	}
-
 	/**
 	 * Used to insert a WordPress styled button, should be paired with a text
 	 * field element.
@@ -252,48 +245,37 @@
 	public function getButton(array $attr = array())
 	{
 		$groupname = isset($attr['groupname']) ? $attr['groupname'] : $this->groupname ;
-
 		$tab = isset($attr['tab']) ? $attr['tab'] : $this->tab ;
-		
+
 		$attr_default = array
 		(
-			'label' => 'Add Media',
-			'href' => $this->getButtonLink($tab),
+			'label' => $this->button_label,
 			'class' => $this->getButtonClass($groupname) . ' button',
 		);
-
 		if (isset($this->insert_button_label))
 		{
-			$attr_default['class'] .= " {label:'" . $this->insert_button_label . "'}";
+			$attr_default['data-update'] = $this->insert_button_label;
 		}
-
 		###
-
 		if (isset($attr['class']))
 		{
 			$attr['class'] = $attr_default['class'] . ' ' . trim($attr['class']);
 		}
-
 		$attr = array_merge($attr_default, $attr);
-
 		$label = $attr['label'];
-
 		unset($attr['label']);
-
 		###
-
 		$elem_attr = array();
-
 		foreach ($attr as $n => $v)
 		{
 			array_push($elem_attr, $n . '="' . $v . '"');
 		}
 
+		$link = $this->getButtonLink();
 		###
-
-		return '<a ' . implode(' ', $elem_attr) . '>' . $label . '</a>';
+		//return '<input type="button" ' . implode(' ', $elem_attr) . ' value="' .$label. '" />';
+		return '<a href="'.$link.'" ' . implode(' ', $elem_attr) . '>'.$label.'</a>';
 	}
-
 	/**
 	 * Used to insert global STYLE or SCRIPT tags into the footer, called on
 	 * WordPress admin_footer action.
@@ -304,111 +286,82 @@
 	 */
 	public function init()
 	{
+		global $post_ID;
 		$uri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : NULL ;
-
 		$file = basename(parse_url($uri, PHP_URL_PATH));
-
 		if ($uri AND in_array($file, array('post.php', 'post-new.php')))
 		{
-			// include javascript for special functionality
-			?><script type="text/javascript">
+
+
+		// Get current post ID
+
+		$post = get_post( $post_ID );
+
+		// Check post type support for thumbnails, call wp_enqueue_media() scripts if thumbnails not supported.
+
+		if(!post_type_supports( $post->post_type, 'thumbnail' ) && function_exists('wp_enqueue_media')) {
+			if(!wp_script_is('custom-header')){
+				wp_enqueue_script('custom-header');
+			}
+			wp_enqueue_media();
+		}
+		// include javascript for special functionality
+			?>
+<script type="text/javascript">
 			/* <![CDATA[ */
-
-				var interval = null;
-
 				jQuery(function($)
 				{
-					if (typeof send_to_editor === 'function')
-					{
 						var wpalchemy_insert_button_label = '';
-
 						var wpalchemy_mediafield = null;
 
-						var wpalchemy_send_to_editor_default = send_to_editor;
+						//Create WP media frame.
 
-						send_to_editor = function(html)
+						var customMediaManager;
+
+						var formlabel = 0;
+						$('[class*=<?php echo $this->button_class_name; ?>]').live('click', function(e)
 						{
-							clearInterval(interval);
+							e.preventDefault();
 
-							if (wpalchemy_mediafield)
-							{
-								var src = html.match(/src=['|"](.*?)['|"] alt=/i);
-								src = (src && src[1]) ? src[1] : '' ;
-
-								var href = html.match(/href=['|"](.*?)['|"]/i);
-								href = (href && href[1]) ? href[1] : '' ;
-
-								var url = src ? src : href ;
-
-								wpalchemy_mediafield.val(url);
-
-								// reset insert button label
-								setInsertButtonLabel(wpalchemy_insert_button_label);
-
-								wpalchemy_mediafield = null;
-							}
-							else
-							{
-								wpalchemy_send_to_editor_default(html);
+							// If the frame already exists, re-open it.
+							if ( customMediaManager ) {
+								customMediaManager.open();
+							return;
 							}
 
-							tb_remove();
-						}
+							// Get our Button element
+							formlabel = jQuery(this);
 
-						function getInsertButtonLabel()
-						{
-							return $('#TB_iframeContent').contents().find('.media-item .savesend input[type=submit], #insertonlybutton').val();
-						}
+							// Get our Form element
+							form = jQuery(this).parent();
 
-						function setInsertButtonLabel(label)
-						{
-							$('#TB_iframeContent').contents().find('.media-item .savesend input[type=submit], #insertonlybutton').val(label);
-						}
+							var customMediaManager = wp.media.frames.customMediaManager = wp.media({
+								 //Title of media manager frame
+								 title: "Upload Document",
+								 library: {
+									type: ''
+								 },
+								 frame: 'select',
+								 button: {
+									//Set Button text
+									text: formlabel.attr("data-update")
+								 },
+								 //Do not allow multiple files, if you want multiple, set true
+								 multiple: false
+							});
+							customMediaManager.on('select', function(){
+								//Set text box value
+								var media_attachment = customMediaManager.state().get('selection').first().toJSON();
+								form.find('input[type="text"]').val(media_attachment.url);
+							});
 
-						$('[class*="<?php echo $this->button_class_name; ?>"]').on('click', function()
-						{
-							var name = $(this).attr('class').match(/<?php echo $this->button_class_name; ?>-([a-zA-Z0-9_-]*)/i);
-							name = (name && name[1]) ? name[1] : '' ;
-
-							var data = $(this).attr('class').match(/({.*})/i);
-							data = (data && data[1]) ? data[1] : '' ;
-							data = eval("(" + (data.indexOf('{') < 0 ? '{' + data + '}' : data) + ")");
-
-							wpalchemy_mediafield = $('.<?php echo $this->field_class_name; ?>-' + name, $(this).closest('.postbox'));
-
-							function iframeSetup()
-							{
-								if ($('#TB_iframeContent').contents().find('.media-item .savesend input[type=submit], #insertonlybutton').length)
-								{
-									// run once
-									if ( ! wpalchemy_insert_button_label.length)
-									{
-										wpalchemy_insert_button_label = getInsertButtonLabel();
-									}
-
-									setInsertButtonLabel((data && data.label)?data.label:'Insert');
-
-									// tab "type" needs a timer in order to properly change the button label
-
-									//clearInterval(interval);
-
-									// setup iframe.load as soon as it becomes available
-									// prevent multiple binds
-									//$('#TB_iframeContent').unbind('load', iframeSetup).bind('load', iframeSetup);
-								}
-							}
-
-							clearInterval(interval);
-
-							interval = setInterval(iframeSetup, 500);
-						});
-					}
+							customMediaManager.open();
+						})
 				});
-
 			/* ]]> */
-			</script><?php
+			</script>
+<?php
 		}
 	}
 }
-
 /* End of file */
